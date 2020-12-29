464abc992b8997f38b564ff03f99900633ee1679
walkerdick2 <68386555+walkerdick2@users.noreply.github.com>
2020-12-29 14:45:02 -0600

    Trying to figure out SQL Servewr Connection

--- Helpdesk System/Server/SMAIN.Designer.vb
+++ Helpdesk System/Server/SMAIN.Designer.vb
@@ -22,13 +22,18 @@ Partial Class SMAIN
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SMAIN))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.TSS1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TSPB1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.DGMAiN = New System.Windows.Forms.DataGridView()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.StatusStrip1.SuspendLayout()
+        CType(Me.DGMAiN, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -43,30 +48,38 @@ Partial Class SMAIN
         '
         'StatusStrip1
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSS1, Me.TSPB1})
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
         Me.StatusStrip1.Name = "StatusStrip1"
         Me.StatusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
         Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
         Me.StatusStrip1.TabIndex = 1
-        Me.StatusStrip1.Text = "StatusStrip1"
         '
-        'ToolStripStatusLabel1
+        'TSS1
         '
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
-        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        Me.TSS1.Name = "TSS1"
+        Me.TSS1.Size = New System.Drawing.Size(0, 17)
         '
-        'ToolStripProgressBar1
+        'TSPB1
         '
-        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
-        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        Me.TSPB1.Name = "TSPB1"
+        Me.TSPB1.Size = New System.Drawing.Size(100, 16)
+        '
+        'DGMAiN
+        '
+        Me.DGMAiN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGMAiN.Location = New System.Drawing.Point(13, 76)
+        Me.DGMAiN.Name = "DGMAiN"
+        Me.DGMAiN.RowTemplate.Height = 25
+        Me.DGMAiN.Size = New System.Drawing.Size(240, 150)
+        Me.DGMAiN.TabIndex = 2
         '
         'SMAIN
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DGMAiN)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -75,6 +88,8 @@ Partial Class SMAIN
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.StatusStrip1.ResumeLayout(False)
         Me.StatusStrip1.PerformLayout()
+        CType(Me.DGMAiN, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -82,6 +97,8 @@ Partial Class SMAIN
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents StatusStrip1 As StatusStrip
-    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
-    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents TSS1 As ToolStripStatusLabel
+    Friend WithEvents TSPB1 As ToolStripProgressBar
+    Friend WithEvents DGMAiN As DataGridView
+    Friend WithEvents BindingSource1 As BindingSource
 End Class
