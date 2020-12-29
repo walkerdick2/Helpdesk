1d2c883da7dfbbbd6893c7d6d04e75c6ef338cea
walkerdick2 <68386555+walkerdick2@users.noreply.github.com>
2020-12-29 13:46:48 -0600

    Icons
    Add  Icons

--- Helpdesk System/Client/My Project/Application.Designer.vb
+++ Helpdesk System/Client/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Client.CMAIN
+            Me.MainForm = Global.Client.Form1
         End Sub
     End Class
 End Namespace
