16facc19706101626cbda4c7d3ea4c5b719ec572
walkerdick2 <68386555+walkerdick2@users.noreply.github.com>
2020-12-29 13:52:51 -0600

    Change Formname

--- Helpdesk System/Server/SMAIN.Designer.vb
+++ Helpdesk System/Server/SMAIN.Designer.vb
@@ -22,9 +22,16 @@ Partial Class SMAIN
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'SMAIN
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "SMAIN"
+        Me.Name = "SMAIN"
+        Me.Text = "Helpdesk Server"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
